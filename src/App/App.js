dea249505a86e48089c54e36338a65011f5be409
Jeffrey Verner <jeffrey@Jeffreys-MacBook-Air.local>
2023-11-27 21:29:04 -0500

    fixing structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import ItemModal from './App/ItemModal';
-
-
-
-
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <div>Date</div>
+        <div>
+          <button type='text'/>
+        </div>
+        <div>Name</div>
+        <div>Avatar Logo</div>
       </header>
     </div>
   );
